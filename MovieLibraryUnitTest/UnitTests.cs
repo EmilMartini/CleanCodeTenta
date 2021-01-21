07d6497ca8e4e160a0b54aa15b37744be6c3afef
emil.martini <emil.martini@contica.se>
2021-01-21 12:06:13 +0100

    add working tests

--- MovieLibraryUnitTest/UnitTests.cs
+++ MovieLibraryUnitTest/UnitTests.cs
@@ -3,9 +3,7 @@ using MovieLibrary;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 
 namespace MovieLibraryUnitTest
 {
@@ -16,7 +14,7 @@ namespace MovieLibraryUnitTest
         public void GetMovieById_ShouldThrowException()
         {
             var logicHandler = new LogicHandler();
-            var testMovies = GetTestProducts();
+            var testMovies = GetTestMovies();
             var testId = "dsak";
 
             Assert.ThrowsException<ArgumentNullException>(() => logicHandler.GetSingleMovie(testMovies, testId));
@@ -26,12 +24,12 @@ namespace MovieLibraryUnitTest
         public void GetMovieById_ShouldSucceed()
         {
             var logicHandler = new LogicHandler();
-            var testMovies = GetTestProducts();
+            var testMovies = GetTestMovies();
             var testId = "tt0111161";
 
             var expected = testMovies.Where(i => i.Id == testId).First();
             var actual = logicHandler.GetSingleMovie(testMovies, testId);
-       
+
             Assert.AreEqual(expected, actual);
         }
 
@@ -39,27 +37,37 @@ namespace MovieLibraryUnitTest
         public void SortMovies_ShouldSortAscending()
         {
             var logicHandler = new LogicHandler();
-            var testMovies = GetTestProducts();
+            var testMovies = GetTestMovies();
+            var expected = GetTestMoviesOrderedAscending();
 
-            var expected = testMovies.OrderBy(i => i.Rated).ToList();
             var actual = logicHandler.SortMovies(testMovies, true);
 
-            CollectionAssert.AreEqual(expected, actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Id, actual[i].Id);
+                Assert.AreEqual(expected[i].Title, actual[i].Title);
+                Assert.AreEqual(expected[i].Rated, actual[i].Rated);
+            }
         }
 
         [TestMethod]
         public void SortMovies_ShouldSortDescending()
         {
             var logicHandler = new LogicHandler();
-            var testMovies = GetTestProducts();
+            var testMovies = GetTestMovies();
+            var expected = GetTestMoviesOrderedDescending();
 
-            var expected = testMovies.OrderByDescending(i => i.Rated).ToList();
             var actual = logicHandler.SortMovies(testMovies, false);
 
-            CollectionAssert.AreEqual(expected, actual);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.AreEqual(expected[i].Id, actual[i].Id);
+                Assert.AreEqual(expected[i].Title, actual[i].Title);
+                Assert.AreEqual(expected[i].Rated, actual[i].Rated);
+            }
         }
 
-        private List<Movie> GetTestProducts()
+        private List<Movie> GetTestMovies()
         {
             var movies = new List<MovieLibrary.Movie>()
             {
@@ -91,14 +99,84 @@ namespace MovieLibraryUnitTest
                 {
                     Title = "The Good, the Bad and the Ugly",
                     Id = "tt0060196",
+                    Rated = double.Parse("8,9")
+                },
+            };
+            return movies;
+        }
+
+        private List<Movie> GetTestMoviesOrderedAscending()
+        {
+            var movies = new List<MovieLibrary.Movie>()
+            {
+                new MovieLibrary.Movie()
+                {
+                    Title = "Pulp Fiction",
+                    Id = "tt0110912",
                     Rated = double.Parse("8,8")
                 },
                 new MovieLibrary.Movie()
                 {
-                    Title = "The Dark Knight",
-                    Id = "tt0468569",
+                    Title = "The Good, the Bad and the Ugly",
+                    Id = "tt0060196",
+                    Rated = double.Parse("8,9")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Godfather: Part II",
+                    Id = "tt0071562",
                     Rated = double.Parse("9,0")
                 },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Godfather",
+                    Id = "tt0068646",
+                    Rated = double.Parse("9,1")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Shawshank Redemption",
+                    Id = "tt0111161",
+                    Rated = double.Parse("9,2")
+                }
+            };
+            return movies;
+        }
+
+        private List<Movie> GetTestMoviesOrderedDescending()
+        {
+            var movies = new List<MovieLibrary.Movie>()
+            {
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Shawshank Redemption",
+                    Id = "tt0111161",
+                    Rated = double.Parse("9,2")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Godfather",
+                    Id = "tt0068646",
+                    Rated = double.Parse("9,1")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Godfather: Part II",
+                    Id = "tt0071562",
+                    Rated = double.Parse("9,0")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "The Good, the Bad and the Ugly",
+                    Id = "tt0060196",
+                    Rated = double.Parse("8,9")
+                },
+                new MovieLibrary.Movie()
+                {
+                    Title = "Pulp Fiction",
+                    Id = "tt0110912",
+                    Rated = double.Parse("8,8")
+                }
             };
             return movies;
         }
